8ace76be9970524e834570a845fd387e39713421
Krishna M <krism.raj@gmail.com>
2026-08-04 14:13:28 +0400

    Updated lint error

--- tests/ui/LoginPageTest.spec.ts
+++ tests/ui/LoginPageTest.spec.ts
@@ -10,7 +10,6 @@ test.describe('Verify Login page elements and functionality', () => {
         signInPage = new SigninPage(page);
     })
 
-
     test('Verify login page elements ',{tag: '@smoke' },async ({ page }) => {
         await page.goto(testData.url);
         await signInPage.validateSignInPageTitle();
